e4501ea1fab649465cab9872e824cf77b3de61bb
moonlander101 <114925949+moonlander101@users.noreply.github.com>
2025-08-12 10:33:42 +0530

    Initialized supabase

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
