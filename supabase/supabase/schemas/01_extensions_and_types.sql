beb69c06a989edbbb4ccad58a28eacf55df2b83c
moonlander101 <114925949+moonlander101@users.noreply.github.com>
2025-08-14 19:15:57 +0530

    Intializing the database and test function

---
+++ supabase/supabase/schemas/01_extensions_and_types.sql
@@ -0,0 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TYPE notification_type AS ENUM ('Confirmation', 'Reminder', 'Update');
+CREATE TYPE send_via_type AS ENUM ('Email', 'SMS', 'Both');
+CREATE TYPE user_role AS ENUM ('Citizen', 'Officer', 'Admin');
+CREATE TYPE appointment_status AS ENUM ('Pending', 'Confirmed', 'Completed', 'Canceled', 'Waited');
+CREATE TYPE verification_status AS ENUM ('Pending', 'Approved', 'Rejected');
