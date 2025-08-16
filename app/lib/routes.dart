768fdaac6f5e3ff563083d5387add6a444728828
Jegatheesan Kavienan <kavienanj@gmail.com>
2025-08-16 22:50:15 +0530

    feat: Implement booking calendar feature with customizable options
    - Added BookingCalendarMain widget to manage the calendar UI and booking slots.
    - Created BookingDialog for loading state representation.
    - Introduced BookingExplanation for displaying booking status colors.
    - Developed BookingSlot for individual time slots with tap functionality.
    - Implemented CommonButton for booking actions with customizable styles.
    - Added CommonCard for consistent card styling across the app.
    - Established BookingController to manage booking logic and state.
    - Created BookingService model to represent booking details.
    - Defined enums for starting day of the week.
    - Added utility functions for booking date manipulations.
    - Integrated booking calendar into the routing system.
    - Created SingleServiceScreen to demonstrate booking functionality.
    - Updated pubspec.yaml and pubspec.lock to include necessary dependencies.

--- app/lib/routes.dart
+++ app/lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:lanka_connect/views/services/screens/single_service_screen.dart';
 import 'models/address.dart';
 import 'views/app.dart';
 import 'views/profile/screens/account_settings.dart';
@@ -119,6 +120,12 @@ final router = GoRouter(
       path: '/public-assistance',
       builder: (context, state) {
         return const PublicAssistanceScreen();
+      },
+    ),
+    GoRoute(
+      path: '/service-booking',
+      builder: (context, state) {
+        return SingleServiceScreen();
       },
     ),
   ],
